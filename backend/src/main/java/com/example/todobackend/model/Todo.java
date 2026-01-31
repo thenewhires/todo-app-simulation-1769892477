b5ab2fde701a082b3a35d5080fef16b0561890dc
thenewhires <the.new.hires.public@gmail.com>
2026-01-31 21:48:03 +0100

    Add backend/src/main/java/com/example/todobackend/model/Todo.java

---
+++ backend/src/main/java/com/example/todobackend/model/Todo.java
@@ -0,0 +1,46 @@
+package com.example.todobackend.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String task;
+    private boolean completed;
+
+    public Todo() {}
+
+    public Todo(String task) {
+        this.task = task;
+        this.completed = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
